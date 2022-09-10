b11c62170407ae0b58e8a2ffffc8b7820e5e464b
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-09-10 12:25:54 +0300

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите начальное число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите начальное число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M={m},N={n},");
+int sum = 0;
+if (m < n)
+
+    for (int i = m + 1; i < n; i++)
+    {
+        sum += i;
+    }
+
+else
+    for (int i = n + 1; i < m; i++)
+    {
+        sum += i;
+    }
+
+Console.Write($"Сумма натуральных элементов в промежутке от M до N = {sum}");
